688732b0122534cb2bb89e984e45321a9afe76e6
tamirmatz <tamirmatz@gmail.com>
2022-01-12 19:52:54 +0200

    comit

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false)
+        });
+        return () => {
+            window.removeEventListener('scroll')
+        }
+    }, [])
+
+
+    return (
+        <div className={`nav ${show && 'nav-black'}`}>
+            <img className='nav-logo' src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg' alt="Netflix logo" />
+            <img className='nav-avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt="Netflix Avatar" />
+        </div>
+    )
+}
+
+export default Nav
